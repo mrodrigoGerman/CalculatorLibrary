62ba411000931197702f6617dd9cb0d9dbf2a5bd
Rodrigo Germán <mrodrigo.german@gmail.com>
2020-12-01 19:34:10 -0300

    Adding a line to the test.

--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,7 @@ class TestCalculator:
 
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
+        assert 7 == calculator.add(1, 4)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
